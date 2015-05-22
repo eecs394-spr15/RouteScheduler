469bc9c86fbf4029ef27f1015db33b6248c951ef
Jon Ford <jondavidford@gmail.com>
2015-05-22 09:55:23 -0500

    move salesperson algorithm to the server (routes.js) and start working on it, need to finish later

--- app/routes.js
+++ app/routes.js
@@ -35,38 +35,112 @@ module.exports = function(app) {
 
 	});
 
-	app.get('/api/salespersons', function(req, res) {
-		// get stuff from database
-		var addresses = [
-			{
-			    "_id": {
-			        "$oid": "555236d3e4b03aad95c1d21f"
-			    },
-			    "name": "Moshe Mcfarland",
-			    "address": "1020 E Nichols RdUnit 4, Palatine, IL, 60074",
-			    "type": "customer"
-			},
-
-			{
-				"_id": {
-			        "$oid": "555236d3e4b03aad95c1d21f"
-			    },
-			    "name": "Moshe Mcfarland",
-			    "address": "1020 E Nichols RdUnit 4, Palatine, IL, 60074",
-			    "type": "customer"
-			},
-
-			{
-					"_id": {
-			        "$oid": "555236d3e4b03aad95c1d21f"
-			    },
-			    "name": "Moshe Mcfarland",
-			    "address": "1020 E Nichols RdUnit 4, Palatine, IL, 60074",
-			    "type": "customer"
-			}
-		]
+	app.get('/api/salesperson-routes', function(req, res) {
+		// add a check right here if the optimized routes are already in the database?
+		// then don't run the algorithm
 
 		// run algorithm
+
+		// there will be a separate task for calculating the distance matrix..
+		// just hard code it for now
+		// this is old stuff
+		//var homeAddresses=[[41.87355847,-87.79641662],[41.98512347,-87.65592603],[ 41.75050329,-87.69003923],[41.74313511, -87.64126162][41.98124939,-87.61216696],[41.77933782,-87.77089982]];
+		//var appointments=[[[ 41.95304104, -87.66079453 ],[41.90056597,-87.4538938],[41.85340177, -87.51506404],[41.81984669,-87.52693874],[ 41.95472842,-87.7796233],[41.97387503,-87.58250468]],
+		//[[ 41.80242171,-87.47432295],[41.87915987,-87.61111856],[41.8855845,-87.55396771],[41.94003661,-87.45577421],[41.78695588,-87.67820154],[41.86954838,-87.65298139]],
+		//[[41.77982066,-87.66223627],[41.89893941, -87.61800847],[41.82592984,-87.76200356],[41.98791955,-87.5361794],[41.81677539,-87.64333341],[41.9154257,-87.69469442]]];
+		//var officeAddress=[41.86928379,-87.5629177];
+
+		// assume n salespeople and m customers
+		// the first n rows of the sales
+		// I am using a full distance matrix because im pretty sure thats how a real non-greedy algorithm would work.
+		// and I don't care if it is more inefficient
+
+		var n = 3;
+		var m = n * 3;
+		var nTotal = n + m + 1; // +1 for the office location
+
+		// initialize stuff needed for algorithm
+		// office is node 0
+		// saleperson homes are nodes 1 -> n + 1
+		// customer locations are nodes n + 1 -> n + 1 + m
+		var salesmanLocation = new Array(n);
+		var routes=new Array(n);
+		for(var i = 0; i < n; i++)
+		{
+			routes[i]=new Array();
+			routes[i].push(0);
+			salesmanLocation[i] = 0; // the starting point is the office
+		}
+		
+		// distance matrix
+		var distanceMatrix = new Array(nTotal);
+  	for (var i = 0; i < nTotal; i++)
+  	{
+    	distanceMatrix[i] = new Array(nTotal);
+  	}
+
+  	// fill distance matrix with garbage
+  	for (var i = 0; i < nTotal; i++)
+  	{
+  		// we only need to fill up half the matrix
+  		for (var j = 0; j <= i; j++)
+  		{
+  			if (i == j)
+  			{
+  				// nodes distance to itself is 0
+  				distanceMatrix[i][j] = 0;
+  			}
+  			else
+  			{
+  				distanceMatrix[i][j] = Math.rand();
+  			}
+  		}
+  	}
+
+		var visited = new Array(m); // only need to mark customer nodes as visited
+		for (var i = 0; i < m; i++) {
+			visited[i] = 0; // nothing is visited
+		}
+
+		// for each time slot
+		for(var appointment=0; appointment < 3; appointment++)//backwards
+		{
+			// for each salesman
+			for(var salesperson = 0; salesperson < n; salesperson++)
+			{
+				// translate to location in distance matrix
+				var translatedSalesperson = salesperson + 1;
+				
+				var origin = salesmanLocation[salesperson];//from last point
+				var minDistance = Infinity;
+				var bestDest = null;
+				
+				// for each customer destination
+				for(var dest = 0; dest < m; dest++){
+					// translate to location in distance matrix
+					var translatedDest = dest + n + 1;
+
+					distance=$scope.calculateDistance(origin,dest);
+					if (distance < minDistance && !visited[dest])
+					{
+						minDistance=distance;
+						bestDest=h;
+
+					}
+				}
+				routes[j].push(appointments[j][bestDest]);
+				visited[bestDest]=1;
+				DistanceForSales[k]+=minDistance;
+
+					
+			}
+			for(var s=0;s<salesNumber;s++)
+			{
+				DistanceForSales+=$scope.calculateDistance(salesmanLocation[s],officeAddress);
+			}
+				
+		}
+		console.log(routes)
 
 		// return json blob of routes
 	})
--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -19,6 +19,10 @@ angular.module('RouteOptimizer', [])
 		this.getAppointments = function() {
 			return $http.get('/api/appointments');
 		};
+
+		this.getOptimizedSalespersonRoutes = function() {
+			return $http.get('/api/salesperson-routes');
+		};
 	}])
 	// inject the Todo service factory into our controller
 	.controller('addEmployeeController', function($scope, $window, EmployeesService) {
@@ -36,66 +40,19 @@ angular.module('RouteOptimizer', [])
 
 	.controller('algorithmController', function($scope, $window, EmployeesService) {
 
-			console.log("hello!");
 			var geocoder = new google.maps.Geocoder();
 			var Locations = [];
 
 			$scope.test = function() {
-				console.log("hello again");
+				
 
-				var salesNumber=6;
-				//var homeAddresses=[[41.87355847,-87.79641662],[41.98512347,-87.65592603],[ 41.75050329,-87.69003923],[41.74313511, -87.64126162][41.98124939,-87.61216696],[41.77933782,-87.77089982]];
-				//var appointments=[[[ 41.95304104, -87.66079453 ],[41.90056597,-87.4538938],[41.85340177, -87.51506404],[41.81984669,-87.52693874],[ 41.95472842,-87.7796233],[41.97387503,-87.58250468]],
-				//[[ 41.80242171,-87.47432295],[41.87915987,-87.61111856],[41.8855845,-87.55396771],[41.94003661,-87.45577421],[41.78695588,-87.67820154],[41.86954838,-87.65298139]],
-				//[[41.77982066,-87.66223627],[41.89893941, -87.61800847],[41.82592984,-87.76200356],[41.98791955,-87.5361794],[41.81677539,-87.64333341],[41.9154257,-87.69469442]]];
-				var salesmanLocation=new Array(6);
-				var routes=new Array(6);
-				var DistanceForSales=[0,0,0,0,0,0];//just store it, may show on the page?
-				var visited;
-				var officeAddress=[41.86928379,-87.5629177];
-
-				for(var i=0;i<salesNumber;i++)
-				{
-					routes[i]=new Array();
-					routes[i].push(homeAddresses[i]);
-					salesmanLocation[i]=homeAddresses[i];//end point
-				}
-
-				for(var j=2;j>=0;j++)//backwards
-				{
-					//for each time slot
-					visited=[0,0,0,0,0,0]
-					for(var k=0;k<salesNumber;k++){
-						//for each salesman
-						var origin=salesmanLocation[k];//from last point
-						var minDistance=1000000;
-						var bestDest;
-						
-						for(var h=0;h<salesNumber;h++){
-							//for each appointment
-							dest=appointments[j][h];
-
-							distance=$scope.calculateDistance(origin,dest);
-							if (distance<minDistance && !visited[h])
-							{
-								minDistance=distance;
-								bestDest=h;
-
-							}
-						}
-						routes[j].push(appointments[j][bestDest]);
-						visited[bestDest]=1;
-						DistanceForSales[k]+=minDistance;
-
-							
-					}
-					for(var s=0;s<salesNumber;s++)
-					{
-						DistanceForSales+=$scope.calculateDistance(salesmanLocation[s],officeAddress);
-					}
-						
-				}
-				console.log(routes)
+				EmployeesService.getOptimizedSalespersonRoutes()
+					.success(function(data, status, headers, config){
+						console.log(data);
+					})
+					.error(function(data, status, headers, config){
+						console.log("Error getting optimized salesperson routes" + status);
+					});
 /*
 				for (var k= salesMan.length-1;k>=0;k--)
 				var minimum=1000;
