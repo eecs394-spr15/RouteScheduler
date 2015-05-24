be96768274bb811071b54ed8034df5149b91c8d2
Bing Wang <lanchuan126@163.com>
2015-05-23 19:08:13 -0500

    move calculatDistance to route.js

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,61 @@
 var Employee = require('./models/employee');
 var Appointment = require('./models/appointment');
+var EARTH_RADIUS = 6378137.0;    //
+var PI = Math.PI;
+var getRad = function(d){
+	return d*PI/180.0;
+}
+var calculateDistance = function(origin, dest) {
+	var lat1=origin[0];
+	var lat2=dest[0];
+	var lng1=origin[1];
+	var lng2=dest[1];
+	var f = getRad((lat1 + lat2)/2);
+    var g = getRad((lat1 - lat2)/2);
+    var l = getRad((lng1 - lng2)/2);
+    
+    var sg = Math.sin(g);
+    var sl = Math.sin(l);
+    var sf = Math.sin(f);
+    
+    var s,c,w,r,d,h1,h2;
+    var a = EARTH_RADIUS;
+    var fl = 1/298.257;
+    
+    sg = sg*sg;
+    sl = sl*sl;
+    sf = sf*sf;
+    
+    s = sg*(1-sl) + (1-sf)*sl;
+    c = (1-sg)*(1-sl) + sf*sl;
+    
+    w = Math.atan(Math.sqrt(s/c));
+    r = Math.sqrt(s*c)/w;
+    d = 2*w*a;
+    h1 = (3*r -1)/2/c;
+    h2 = (3*r +1)/2/s;
+    
+    return d*(1 + fl*(h1*sf*(1-sg) - h2*(1-sf)*sg));
+/*
+	lat1=origin[0];
+	lat2=dest[0];
+	lon1=origin[1];
+	lon2=dest[1];
+	var R = 6371000; // metres
+	var φ1 = lat1*Math.PI/180;
+	var φ2 = lat2*Math.PI/180;
+	var Δφ = (lat2-lat1)*Math.PI/180;
+	var Δλ = (lon2-lon1)*Math.PI/180;
 
+	var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+	        Math.cos(φ1) * Math.cos(φ2) *
+	        Math.sin(Δλ/2) * Math.sin(Δλ/2);
+	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+	var d = R * c;
+	return d;
+	*/
+	}
 module.exports = function(app) {
 
 	// api
